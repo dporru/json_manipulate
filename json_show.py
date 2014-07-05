accdf7be0e12ff907be02cdb8e4216fb2a0b5086
dporru <daanporru@gmail.com>
2014-07-05 08:59:23 +0200

    initial commit

---
+++ json_show.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import json, sys, argparse
+
+def manipulate(json_object, manipulate_string):
+   (key, rest) = get_key(manipulate_string)
+   
+def get_key(manipulate_string):
+    return None
+
+def get_args(argument_list):
+    parser = argparse.ArgumentParser(description='Show and manipulate json strings.')
+    parser.add_argument('-m', '--manipulate_string', type=str, default='', help='A maniputation string, see description.')
+    return parser.parse_args(argument_list[1:])
+
+if __name__ == '__main__':
+    json_object = json.load(sys.stdin)
+    
+    args = get_args(sys.argv)
+    
+    json_object = manipulate(json_object, args.manipulate_string)
+    
+    print json.dumps(json_object, indent=4, separators=(',', ': '))
+
+# ''
+# 'response.result.(persons|recepies)'
+# 'response.result.persons[firstname|last_name]'
+# 'response.result.persons[firstname|last_name|(address.street|number)]'
+# 'response.result.persons[firstname]'
---
+++ tests/test_json_show.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import sys
+sys.path.append('../')
+from json_show import json_show
+import unittest
+
+class TestJsonShow(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_args_no_arguments(self):
+        argument_list = ['json_show.py']
+        
+        args = json_show.get_args(argument_list)
+        
+        self.assertEqual(args.manipulate_string, '')
+
+    def test_get_args_manipulate_string(self):
+        argument_list = ['json_show.py', '-m', 'result']
+        
+        args = json_show.get_args(argument_list)
+        
+        self.assertEqual(args.manipulate_string, 'result')
+    
+    def test_get_key_empty_string(self):
+        manipulate_string = ''
+        
+        result = json_show.get_key(manipulate_string)
+        
+        self.assertEqual(result, None)
+        
+if __name__ == '__main__':
+    unittest.main()
