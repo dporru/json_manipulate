aca449ca276fd47fe33931dbd02cee3ec68b074e
dporru <daanporru@gmail.com>
2014-07-06 16:31:54 +0200

    Added comments to the code

--- json_show.py
+++ json_show.py
@@ -2,6 +2,26 @@
 import json, sys, argparse
 
 def manipulate(json_object, key, rest):
+    """ 
+    Manipulate a dictionary.
+
+    Args:
+        json_object: dictionary or list to manipulate
+        key:
+            1) string            -> select only given key from dictionary
+            2) list of strings   -> select only given keys from dictionary
+                                    or select only givens keys from the dictionaries
+                                    in the given list
+            3) None              -> return given dictionary or list untouched
+        rest:
+            1) tuple (key, rest) -> recursively call manipulate on the by key selected
+                                    part of the dictionary or list
+            2) None              -> end recursive calls
+    Returns:
+        Manipulated dictonary or list
+    """
+    
+    # key is string: select key from dictionary
     if type(key) == str:
         try:
             if rest != None:
@@ -11,13 +31,16 @@ def manipulate(json_object, key, rest):
             return {key : rest_object}
         except KeyError:
             raise KeyNotFound(key)
-        
+    
+    # key is list: select keys from dictionary or keys from list of dictonaries
     if type(key) == list:
+        # select keys from list of dictionaries
         if type(json_object) == list:
             return_list = []
             for item in json_object:
                 return_list.append(manipulate(item, key, rest))
             return return_list
+        # select keys from dictionary
         else:
             return_object = {}
             for sub_key in key:
@@ -28,6 +51,19 @@ def manipulate(json_object, key, rest):
        return json_object
    
 def get_key(ms):
+    """
+    Creates a key and a rest tuple based on a manipulation string
+    
+    Args:
+        ms: Manipulation string
+            eg. '', 'result','result.rows' or 'result.rows[name|address]'
+    Returns:
+        A tuple of key and rest. In the above example respectivly:
+            (None, None)
+            ('result', None)
+            ('result', ('rows', None))
+            ('result', ('rows', (['name', 'address'], None)))
+    """
     if ms == '':
         return (None, None)
     
@@ -62,6 +98,15 @@ def get_key(ms):
         return (ms, None)
 
 def get_piped_parts(ms):
+    """
+    Splits a string on the pipe '|' character, but only if it
+    is not inside parenthesis () or square brackets [].
+    
+    Args:
+        ms: Manipulation string
+    Returns:
+        List of strings
+    """
     if ms == '':
         return []
         
@@ -75,6 +120,9 @@ def get_piped_parts(ms):
     return [ms[:pipe_pos]] + get_piped_parts(ms[pipe_pos+1:])
 
 def get_args(argument_list):
+    """
+    Returns an object with command line arguments.
+    """
     parser = argparse.ArgumentParser(description='Show and manipulate json strings.')
     parser.add_argument('-m', '--manipulate_string', type=str, default='', help='A maniputation string, see description.')
     return parser.parse_args(argument_list[1:])
@@ -86,12 +134,17 @@ class KeyNotFound(Exception):
         return repr(self.value)
 
 if __name__ == '__main__':
+    # load the json_object from the stdin
     json_object = json.load(sys.stdin)
     
+    # parse command line arguments
     args = get_args(sys.argv)
     
+    # create json manipulation object based on command line arguments
     (key, rest) = get_key(args.manipulate_string)
     
+    # create the manipulated json object
     json_object = manipulate(json_object, key, rest)
     
+    # print the manipulated json object to screen
     print json.dumps(json_object, indent=4, separators=(',', ': '))
